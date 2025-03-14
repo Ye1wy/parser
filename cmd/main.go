763cdab254d960770840b42a523e88b5728374ba
jenconn <ye1wyy@gmail.com>
2025-03-14 20:44:49 +0700

    update: change tactics to scrap samokat website

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
 )
 
@@ -13,6 +15,9 @@ func main() {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", true),
 		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("disable-blink-features", "AutomationControlled"),
 		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"),
 	)
 
@@ -22,20 +27,51 @@ func main() {
 	ctx, cancel = chromedp.NewContext(ctx)
 	defer cancel()
 
-	// url := "https://lenta.com"
-	url := "https://samokat.ru"
+	originUrl := "https://samokat.ru"
+	url := originUrl + "/category/vsya-gotovaya-eda-6"
 	fmt.Println("trying to enter...")
 
 	var html string
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(url),
-		chromedp.Sleep(10*time.Second),
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
 		chromedp.OuterHTML("html", &html),
 	)
 
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	fmt.Println("Website loaded successfully:", html)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Fatalf("Failed to parse HTML: %v", err)
+	}
+
+	// maybe worst: if classes in html is changes then scraping needed update
+	productNameCard := ".ProductCard_name__2VDcL"
+	productPriceCard := ".ProductCardActions_text__3Uohy"
+	productLinkClass := "a[href^='/product/']"
+
+	var productNames, productPrices, productLinks []string
+
+	doc.Find(productNameCard).Each(func(i int, s *goquery.Selection) {
+		productNames = append(productNames, strings.TrimSpace(s.Text()))
+	})
+
+	doc.Find(productPriceCard).Each(func(i int, s *goquery.Selection) {
+		productPrices = append(productPrices, strings.TrimSpace(s.Text()))
+	})
+
+	doc.Find(productLinkClass).Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists {
+			productLinks = append(productLinks, originUrl+href)
+		}
+	})
+
+	fmt.Println("Extracted data: ")
+	fmt.Println(productNames)
+	fmt.Println(productPrices)
+	fmt.Println(productLinks)
 }
