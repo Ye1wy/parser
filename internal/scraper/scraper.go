a43a89166fad9dac5a17f6482f3fd8512472d683
jenconn <ye1wyy@gmail.com>
2025-03-17 18:52:31 +0700

    update: added interface for expansion and an object SamokatScraper has been created for dependency inversion

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,1 +1,64 @@
 package scraper
+
+import (
+	"context"
+	"log/slog"
+	"parser/config"
+	"parser/internal/logger"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+type Scraper interface {
+	ScrapeCategory(url string) (string, error)
+}
+
+type SamokatScraper struct {
+	Config config.ConfigProvider
+	logger *slog.Logger
+}
+
+func NewSamokatScraper(cfg config.ConfigProvider, log *slog.Logger) *SamokatScraper {
+	return &SamokatScraper{
+		Config: cfg,
+		logger: log,
+	}
+}
+
+func (ss *SamokatScraper) ScrapeCategory(url string) (string, error) {
+	op := "scraper.SamokatScraper.ScraperCategory"
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", ss.Config.GetOptHeadless()),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("disable-blink-features", "AutomationControlled"),
+		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"),
+	)
+
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	ss.logger.Info("Created chromedp new context", "op", op)
+
+	ss.logger.Info("trying to enter... ", "op", op)
+
+	var html string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
+		chromedp.Sleep(ss.Config.GetRequestDelay()*time.Second),
+		chromedp.OuterHTML("html", &html),
+	)
+
+	if err != nil {
+		ss.logger.Error("Error in run to website: ", logger.Err(err), "op", op)
+		return "", err
+	}
+
+	return html, nil
+}
