7615cebcbcaae0a83228d4207072819f877779fb
jenconn <ye1wyy@gmail.com>
2025-03-17 18:53:51 +0700

    add: added parser html page

---
+++ internal/scraper/parser.go
@@ -0,0 +1,73 @@
+package scraper
+
+import (
+	"log/slog"
+	"parser/config"
+	"parser/internal/logger"
+	"parser/internal/models"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type ParserHTML interface {
+	ParseHTML(html string)
+}
+
+type SamokatParser struct {
+	config config.ConfigProvider
+	logger *slog.Logger
+}
+
+func NewSamokatParser(cfg config.ConfigProvider, log *slog.Logger) *SamokatParser {
+	return &SamokatParser{
+		config: cfg,
+		logger: log,
+	}
+}
+
+func (sp *SamokatParser) ParseHTML(html string) ([]models.Product, error) {
+	op := "scraper.SamokatParser.ParseHTML"
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		sp.logger.Error("Failed to parse HTML: %v", logger.Err(err), "op", op)
+		return nil, err
+	}
+
+	// maybe worst: if classes in html is changes then scraping needed update
+	productNameCard := ".ProductCard_name__2VDcL"
+	productPriceCard := ".ProductCardActions_text__3Uohy"
+	productLinkClass := "a[href^='/product/']"
+
+	var products []models.Product
+
+	var productNames, productPrices, productLinks []string
+
+	doc.Find(productNameCard).Each(func(i int, s *goquery.Selection) {
+		productNames = append(productNames, strings.TrimSpace(s.Text()))
+	})
+
+	doc.Find(productPriceCard).Each(func(i int, s *goquery.Selection) {
+		productPrices = append(productPrices, strings.TrimSpace(s.Text()))
+	})
+
+	doc.Find(productLinkClass).Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists {
+			productLinks = append(productLinks, "https://samokat.ru/"+href)
+		}
+	})
+
+	for i := 0; i <= len(productNames) && i <= len(productPrices) && i <= len(productLinks); i++ {
+		product := models.Product{
+			Name:  productNames[i],
+			Price: productPrices[i],
+			Link:  productLinks[i],
+		}
+
+		products = append(products, product)
+	}
+
+	return products, nil
+}
